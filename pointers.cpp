1b670ef8bce5d1147bc3acd343227c5ec94ea9c9
anass CHOUKRI <38406227+anasschoukri2@users.noreply.github.com>
2020-01-19 19:09:28 +0100

    Create pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    cout<<a+b<<endl<<abs(a-b);
+
+    return 0;
+}
+
