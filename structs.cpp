e8dff92b67ce7f13ec16093f6093e4e33c41ee97
anass CHOUKRI <38406227+anasschoukri2@users.noreply.github.com>
2020-01-19 18:54:54 +0100

    Create structs.cpp

---
+++ structs.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+    add code for struct here.
+*/
+
+int main() {
+    string s;
+    cin>>s;
+    while(!cin.fail()){
+        cout<<s<<" ";
+        cin>>s;
+    }
+    
+    return 0;
+}
+
