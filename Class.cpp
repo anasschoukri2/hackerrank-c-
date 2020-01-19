763835c5f30ebd9e0f9f2a88419b0c90df255975
anass CHOUKRI <38406227+anasschoukri2@users.noreply.github.com>
2020-01-19 19:05:41 +0100

    Create Class.cpp

---
+++ Class.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int age, standard;
+    string first_name, last_name;
+    cin >> age >> first_name >> last_name >> standard;
+    cout << age << endl << last_name << ", " << first_name << endl << standard << endl << endl;
+    cout << age << "," << first_name << "," << last_name << "," << standard;
+    return 0;
+}
